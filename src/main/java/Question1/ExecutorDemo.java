a23a703ee7b4990c0967ea4c043bc0ea4d67530e
SalilKnols <salil.verma@knoldus.com>
2023-03-13 17:28:20 +0530

    Executor Demo Program

---
+++ src/main/java/Question1/ExecutorDemo.java
@@ -0,0 +1,31 @@
+package Question1;
+
+import java.util.concurrent.*;
+
+class Task implements Runnable {
+    private int value;
+
+    public Task(int value) {
+        this.value = value;
+    }
+
+    public void run() {
+        System.out.println("Value: " + value);
+    }
+}
+
+public class ExecutorDemo {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        for (int index = 0; index < 10; index++) {
+            Task task = new Task(index);
+            Future<?> future = executor.submit(task);
+            try {
+                future.get();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
